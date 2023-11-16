02b0a938f753697132ea9cce821ece4e7bec6b84
JacobChurch2 <jchurch@student.neumont.edu>
2023-11-15 18:09:35 -0700

    comments

--- Pro100Project/GameBoard.h
+++ Pro100Project/GameBoard.h
@@ -3,16 +3,16 @@ namespace Umbrella {
 	class GameBoard {
 	public:
 		GameBoard() {
-			/*for (int i = 0; i < 10; i++)
+			for (int i = 0; i < 10; i++)
 			{
 				for (int j = 0; j < 10; j++)
 				{
 					board[i][j] = 'x';
 				}
-			}*/
+			}
 		}
 	public:
-		char board[10][10] = {	{'x','x','x','x','x','x','x','x','x','x'},
+		char board[10][10]; /*= {	{'x','x','x','x','x','x','x','x','x','x'},
 								{'x','x','x','x','x','x','x','x','x','x'},
 								{'x','x','x','x','x','x','x','x','x','x'},
 								{'x','x','x','x','x','x','x','x','x','x'},
@@ -21,7 +21,7 @@ namespace Umbrella {
 								{'x','x','x','x','x','x','x','x','x','x'},
 								{'x','x','x','x','x','x','x','x','x','x'},
 								{'x',' ','x','x','x','x','x','x','x','x'},
-								{' ','x','x','x','x','x','x','x','x','x'} };
+								{' ','x','x','x','x','x','x','x','x','x'} };*/
 		void ClearLines();
 	private:
 		void ClearLine(int col);
