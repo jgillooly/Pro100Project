4084207227fa62a899bdc35dedd18be81bdc1552
JacobChurch2 <jchurch@student.neumont.edu>
2023-11-22 18:27:25 -0700

    hehe

--- Pro100Project/Game.cpp
+++ Pro100Project/Game.cpp
@@ -1,7 +1,31 @@
 #include "Game.h"
 
 namespace Umbrella {
-	void Game::run() {
+	std::vector<int> Umbrella::Game::GetRandomPiece()
+	{
+		int output = 1 + (rand() * (int)(7 - 1) / RAND_MAX);
+		switch (output) {
+		case 1:
+			return oBlock;
+		case 2:
+			return iBlock;
+		case 3:
+			return sBlock;
+		case 4:
+			return zBlock;
+		case 5:
+			return tBlock;
+		case 6:
+			return jBlock;
+		case 7:
+			return lBlock;
+		}
+	}
 
+	void Game::play() {
+		bool quit = false;
+		do {
+
+		} while (!quit);
 	}
 }
