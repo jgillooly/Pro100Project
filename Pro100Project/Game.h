c82ec1887bf55eeced31e2d18a36c158c1c3368c
JacobChurch2 <jchurch@student.neumont.edu>
2023-11-29 18:30:53 -0700

    Color but no color, with some rotated blocks

--- Pro100Project/Game.h
+++ Pro100Project/Game.h
@@ -6,13 +6,26 @@
 namespace Umbrella {
     class Game {
     public:
-        std::vector<int> oBlock = { 0, 0, 0, 1, 1, 0, 1, 1 };
-        std::vector<int> iBlock = { 0,0,0,1,0,2,0,3 };
-        std::vector<int> sBlock = {2,0,1,0,1,1,0,1};
-        std::vector<int> zBlock = {0,0,1,0,1,1,2,1};
-        std::vector<int> tBlock = {0,0,1,0,1,1,2,0};
-        std::vector<int> jBlock = {0,0,1,0,2,0,2,1};
-        std::vector<int> lBlock = {0,1,1,1,2,1,2,0};
         std::vector<int> GetRandomPiece();
+
+        std::vector<int> oBlock = { 0, 0, 0, 1, 1, 0, 1, 1 };
+
+        std::vector<int> iBlock = { 0,0,0,1,0,2,0,3 };
+        std::vector<int> iBlockR = { 0,0,1,0,2,0,3,0 };
+
+        std::vector<int> sBlock = { 1,0,1,1,0,1,0,2 };
+        std::vector<int> sBlockR = { 0,0,1,0,1,1,2,1 };
+
+        std::vector<int> zBlock = { 0,0,0,1,1,1,1,2 };
+        std::vector<int> zBlockR = { 2,0,1,0,1,1,0,1 };
+
+        std::vector<int> tBlock = { 0,0,1,0,1,1,2,0 };
+        std::vector<int> tBlockR = { 2,0,2,1,1,1,2,2 };
+
+        std::vector<int> jBlock = { 0,0,1,0,2,0,2,1 };
+        std::vector<int> jBlockR = { 0,2,0,1,0,2,0,2,1 };
+
+        std::vector<int> lBlock = { 0,1,1,1,2,1,2,0 };
+        std::vector<int> lBlockR = { 0,1,1,1,2,1,2,2 };
 	};
 }
